6a71844880135aaa5d5461ffcc085f9262af1ebc
Raj Ukani <rajukani100@gmail.com>
2024-12-12 21:46:37 +0530

    designed login page

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,8 +1,21 @@
+import { buttonVariants } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
-        <div>Login</div>
-    )
-}
+        <div className="w-full h-screen flex items-center justify-center" >
+            <div className="w-1/3 h-fit p-6 flex flex-col items-center justify-center bg-white shadow-md rounded-lg border-[1px]" >
+                <div className="text-3xl mb-4" > Login </div>
 
-export default Login
+                <Input type="email" placeholder="Email" className="mb-4 focus-visible:ring-transparent" />
+                <Input type="password" placeholder="Password" className="mb-4 focus-visible:ring-transparent" />
+                <Link className={buttonVariants({ variant: "outline", className: "bg-cyan-400 hover:bg-cyan-300 font-bold text-white hover:text-white w-full" })} to={"/"} > Submit </Link>
+
+
+            </div>
+        </div>
+    );
+};
+
+export default Login;
--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Register = () => {
     return (
         <div className= "w-full h-screen flex items-center justify-center" >
-        <div className="w-1/3 h-fit p-6 flex flex-col items-center justify-center bg-white shadow-md rounded-lg" >
+        <div className="w-1/3 h-fit p-6 flex flex-col items-center justify-center bg-white shadow-md rounded-lg border-[1px]" >
             <div className="text-3xl mb-4" > Register </div>
 
                 < Input type = "text" placeholder = "Name" className = "mb-4 focus-visible:ring-transparent" />
