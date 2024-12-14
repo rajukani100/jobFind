b80ffa091f0586a5712a9477274d05d3cb36f1bb
Raj Ukani <rajukani100@gmail.com>
2024-12-14 16:18:36 +0530

    changes in input field

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -4,19 +4,18 @@ import { Link } from "react-router-dom";
 
 const Register = () => {
     return (
-        <div className= "w-full h-screen flex items-center justify-center" >
-        <div className="w-1/3 h-fit p-6 flex flex-col items-center justify-center bg-white shadow-md rounded-lg border-[1px]" >
-            <div className="text-3xl mb-4" > Register </div>
+        <div className="w-full h-screen flex items-center justify-center" >
+            <div className="w-1/3 h-fit p-6 flex flex-col items-center justify-center bg-white shadow-md rounded-lg border-[1px]" >
+                <div className="text-3xl mb-4" > Register </div>
 
-                < Input type = "text" placeholder = "Name" className = "mb-4 focus-visible:ring-transparent" />
-                    <Input type="email" placeholder = "Email" className = "mb-4 focus-visible:ring-transparent" />
-                        <Input type="text" placeholder = "Age" className = "mb-4 focus-visible:ring-transparent" />
-                            <Input type="password" placeholder = "Password" className = "mb-4 focus-visible:ring-transparent" />
-                                <Link className={ buttonVariants({ variant: "outline", className: "bg-cyan-400 hover:bg-cyan-300 font-bold text-white hover:text-white w-full" }) } to = { "/"} > Submit </Link>
+                < Input type="text" placeholder="Name" className="mb-4 focus-visible:ring-transparent" />
+                <Input type="email" placeholder="Email" className="mb-4 focus-visible:ring-transparent" />
+                <Input type="password" placeholder="Password" className="mb-4 focus-visible:ring-transparent" />
+                <Link className={buttonVariants({ variant: "outline", className: "bg-cyan-400 hover:bg-cyan-300 font-bold text-white hover:text-white w-full" })} to={"/"} > Submit </Link>
 
 
-                                    </div>
-                                    </div>
+            </div>
+        </div>
     );
 };
 
