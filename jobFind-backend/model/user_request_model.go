2f6e8cdbcb38cbad0c7e1b1e09f6b5364ea39a3d
Raj Ukani <rajukani100@gmail.com>
2024-12-15 23:14:00 +0530

    removed file

--- jobFind-backend/model/user_request_model.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type UserRequest struct {
-	Name     string `json:"name" binding:"required"`
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required"`
-}
