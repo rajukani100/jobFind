e1902f1f3476a908e8454fdd73b20a6cbea60631
Raj Ukani <rajukani100@gmail.com>
2024-12-15 23:14:22 +0530

    added update method

--- jobFind-backend/database/token.go
+++ jobFind-backend/database/token.go
@@ -7,3 +7,9 @@ func SaveJwtTokens(userId, accessToken, refreshToken string) error {
 	_, err := ConnPool.Exec(context.Background(), query, userId, accessToken, refreshToken)
 	return err
 }
+
+func UpdateJwtTokens(userId, accessToken, refreshToken string) error {
+	query := `UPDATE public.tokens SET accesstoken = $2 , refreshtoken = $3 WHERE userid = $1;`
+	_, err := ConnPool.Exec(context.Background(), query, userId, accessToken, refreshToken)
+	return err
+}
