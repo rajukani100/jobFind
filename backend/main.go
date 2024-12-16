8134b1917c1ac0a404e2aeb7854d401051029fe8
Raj Ukani <rajukani100@gmail.com>
2024-12-16 12:03:03 +0530

    added CORS config

--- backend/main.go
+++ backend/main.go
@@ -4,12 +4,24 @@ import (
 	"jobfind/controllers"
 	databse "jobfind/database"
 	"log"
+	"time"
+
+	"github.com/gin-contrib/cors"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.New()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"}, // Allow specific origin
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Accept"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 
