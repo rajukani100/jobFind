817848c12f58b57bbfbb95cbfb9b7e7b8eabc7ed
Raj Ukani <rajukani100@gmail.com>
2024-12-30 19:38:56 +0530

    added /job/ route

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/home/home";
+import Job from "./pages/job/job";
 import Login from "./pages/login/login";
 import AboutPage from "./pages/about/about";
 import Register from "./pages/register/register";
+import AuthProvider from "./contexts/authProvider";
 
 function App() {
+
+
+
   return (
     <>
       <div className='w-full'>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/about' element={<AboutPage />} />
-            <Route path='/' element={<HomePage />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/login' element={<Login />} />
-          </Routes>
-        </BrowserRouter>
+        <AuthProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/job/:id' element={<Job />} />
+              <Route path='/about' element={<AboutPage />} />
+              <Route path='/' element={<HomePage />} />
+              <Route path='/register' element={<Register />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='*' element={<HomePage />} />
+            </Routes>
+          </BrowserRouter>
+        </AuthProvider>
+
       </div>
     </>
   );
